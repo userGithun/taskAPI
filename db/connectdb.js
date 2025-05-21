2effffaea98896ffddc82c45637a792c93d8ab6a
Harsh verma <chacha45454@gmail.com>
2025-05-21 12:32:39 +0530

    live

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const local_url = 'mongodb://127.0.0.1:27017/taskAPI'
-
+const live_URL = 'mongodb+srv://chacha45454:ram123@cluster0.edlqo.mongodb.net/reactProductsAPI?retryWrites=true&w=majority&appName=Cluster0'
 
 const connectdb = () => {
-    return mongoose.connect(local_url)
+    return mongoose.connect(live_URL)
         .then(() => {
             console.log('Data-base connected!')
         })
